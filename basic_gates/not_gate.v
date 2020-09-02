e84183e60244eb0d6f7e89e0002c9fde30f3e698
k-sva <23096719+PXVI@users.noreply.github.com>
2020-09-03 00:40:42 +0530

    Update not_gate.v

--- basic_gates/not_gate.v
+++ basic_gates/not_gate.v
@@ -1,9 +1,9 @@
 /* -----------------------------------------------------------------------------------
- * Module Name  :
+ * Module Name  : not_gate
  * Date Created : 00:18:20 IST, 03 September, 2020 [ Thursday ]
  *
  * Author       : pxvi
- * Description  :
+ * Description  : Basic parameterized NOT gate
  * ----------------------------------------------------------------------------------- */
 
 module not_gate#( parameter WIDTH = 1 )(    input [WIDTH-1:0] IN0,
