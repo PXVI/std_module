9419d02d5845cdfa70814e40e22ad294ece5178c
padmakar <pxvi@github.com>
2020-09-03 00:29:38 +0530

    Added the basic gates modules

---
+++ basic_gates/not_gate.v
@@ -0,0 +1,38 @@
+/* -----------------------------------------------------------------------------------
+ * Module Name  :
+ * Date Created : 00:18:20 IST, 03 September, 2020 [ Thursday ]
+ *
+ * Author       : pxvi
+ * Description  :
+ * ----------------------------------------------------------------------------------- */
+
+module not_gate#( parameter WIDTH = 1 )(    input [WIDTH-1:0] IN0,
+                                            output [WIDTH-1:0] OUT0 );
+
+    assign OUT0 = ~( IN0 );
+
+endmodule
+
+// -----------------------------------------------------------------------------------
+// Testbench
+// -----------------------------------------------------------------------------------
+
+module not_gate_top;
+
+    reg I0;
+    wire Out;
+
+    not_gate gate0( I0, Out );
+
+    initial
+    begin
+        $display( "I0 : %b, Out : %b", I0, Out );
+        I0 = 0;
+        #1;
+        $display( "I0 : %b, Out : %b", I0, Out );
+        I0 = 1;
+        #1;
+        $display( "I0 : %b, Out : %b", I0, Out );
+    end
+    
+endmodule
---
+++ basic_gates/xnor_gate.v
@@ -0,0 +1,49 @@
+/* -----------------------------------------------------------------------------------
+ * Module Name  : xnor_gate
+ * Date Created : 00:18:20 IST, 03 September, 2020 [ Thursday ]
+ *
+ * Author       : pxvi
+ * Description  : Basic parameterized XNOR gate
+ * ----------------------------------------------------------------------------------- */
+
+module xnor_gate#( parameter WIDTH = 1 )(   input [WIDTH-1:0] IN0,
+                                            input [WIDTH-1:0] IN1,
+                                            output [WIDTH-1:0] OUT0 );
+
+    assign OUT0 = ~( IN0 ^ IN1 );
+
+endmodule
+
+// -----------------------------------------------------------------------------------
+// Testbench
+// -----------------------------------------------------------------------------------
+
+module xnor_gate_top;
+
+    reg I0, I1;
+    wire Out;
+
+    xnor_gate gate0( I0, I1, Out );
+
+    initial
+    begin
+        $display( "I0 : %b, I1 : %b, Out : %b", I0, I1, Out );
+        I0 = 0;
+        I1 = 0;
+        #1;
+        $display( "I0 : %b, I1 : %b, Out : %b", I0, I1, Out );
+        I0 = 0;
+        I1 = 1;
+        #1;
+        $display( "I0 : %b, I1 : %b, Out : %b", I0, I1, Out );
+        I0 = 1;
+        I1 = 0;
+        #1;
+        $display( "I0 : %b, I1 : %b, Out : %b", I0, I1, Out );
+        I0 = 1;
+        I1 = 1;
+        #1;
+        $display( "I0 : %b, I1 : %b, Out : %b", I0, I1, Out );
+    end
+    
+endmodule
